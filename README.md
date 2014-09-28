cd3618e2ab8c1b7130bb2aab53a94701aa496117
Leigh Zhu <i@zhuli.me>
2014-09-28 15:42:50 +0800

    add readme paths collector

---
+++ README.md
@@ -0,0 +1,11 @@
+# dep-docs (in dev... now)
+a simple tools to collect node modules's docs(readme) in your project.
+
+
+# API sample
+
+```js
+var depDocs = requrie('dep-docs');
+
+var docs = depDocs('./');
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+var path = require('path');
+var fs = require('fs');
+
+var resolveDep = require('resolve-dep');
+
+module.exports = exports.depDocs = depDocs;
+
+function depDocs(_path, options) {
+    if(_path.indexOf('node_modules') < 0) {
+        _path = path.resolve(_path, 'node_modules/');
+    }
+
+    return resolveDep(_path + '/*', options).map(function(dep) {
+        return path.resolve(dep, fs.readdirSync(dep).filter(function(file){
+            return /^readme/.test(file.toLowerCase());
+        })[0] || '');
+    });
+}
